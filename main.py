345f13a4024210aa2f5a17e92f82812046a8d688
111diana111 <diana_serpoianu@yahoo.com>
2024-06-25 10:56:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-import json
 from flask import Flask, jsonify, request
-from uagents import Agent, Context, Model, Bureau, Protocol
+from uagents import Agent, Context, Model, Protocol
 from uagents.setup import fund_agent_if_low
-from uagents.query import query
-import requests
-import os
-import jsonify
-from flask_cors import CORS
+from datetime import *
+from random import uniform
+from chemspipy import ChemSpider
+import json
 
-app=Flask(__name__)
-CORS(app)
 
-# Define addresses:
+cs = ChemSpider('8L0LTGCA64VhQnlwKB9OPJcpVGN6XKqy')
 
-skin_address = "agent1q2u49qvajqzpds9yhm5f3y8wz86dhumxmqlusa6rxmxg557uj5sw55p844t"
-user_address ="agent1q0wzslgquuhfykzsyj053jrtuf5vu7caftm7ghjv0vc7v8n0q8x25nuswcq"
-product_address ="agent1qwmdwqt5adeduup748zvc0mfzxat3r4a3ssh5yk0fuzertza6df5yr4x2qy"
-environment_address ="agent1qvd29utqf756g8tlc3d7gxnvaxw8v86wnhx0u9ccd5kdesd39jh6z577phz"
-viscosity_address ="agent1qvn9fzuzadjx3djsucwlkpn4w8wcpdutjtkt5ljjlksntvqqwk3ky9anm2y"
-density_address = "agent1qtr5u00y2vhdprja5efvl6afa92zqtapr9u4lwg4vgq4njq8zx9wvmx0f3q"
-#DoctorAgent.address ="agent1qtu3xyt9m0a4pdzpw4ehj0a8mjj3agfp0vmh0a6skh3w04w02vpkqnn9vjv"
 
-# Define Request Response, and Error messages for the Skin App Agent:
+#Define User Request and response
+
+class SkinStatus(Model):
+    hydration: int
+    elasticity: float
+    collagen_density: float
+    day: str
+
+class QuerySkinRequest(Model):
+    day: str
+    brand: str
+    product: str
+
+class QuerySkinResponse(Model):
+    day: str
+    brand: str
+    product: str
+    hydration: float
+    elasticity: float
+    collagen_density: float
+    absorption_speed: float
+    absorption_level: float
+
+class ErrorResponse(Model):
+    error: str
+
+#Define SkinApp Request and Response
 class DefaultData(Model):
     day: str
     hydration: float
@@ -40,11 +55,11 @@ class DefaultData(Model):
     humidity: float
 
 
-class DataRequest(Model):
+class AppDataRequest(Model):
     day: str
 
 
-class DataResponse(Model):
+class AppDataResponse(Model):
     day: str
     hydration: float
     elasticity: float
@@ -61,12 +76,21 @@ class DataResponse(Model):
     temperature: float
     humidity: float
 
-class ErrorResponse(Model):
+class AppErrorResponse(Model):
     error: str
 
-# Define Request, Response and Error Models for the Product Agent
+class ProductFormula(Model):
+    day: str
+    brand: str
+    product: str
+    ingredients: list
+    formula: list
+
+
 class FormulaRequest(Model):
-    product_name: str
+    brand: str
+    product: str
+    day: str
 
 class FormulaResponse(Model):
     ingredients: list
@@ -74,62 +98,7 @@ class FormulaResponse(Model):
 class ErrorResponse(Model):
     error: str
 
-
-# Define Request, Response and Error Models for the Density App
-class DensityRequest(Model):
-    product_name: str
-
-
-class DensityResponse(Model):
-    density: float
-
-
-class ErrorResponse(Model):
-    error: str
-
-# # Define Request, Response and Error Models for the Viscosity App
-
-class ViscosityRequest(Model):
-    product_name: str
-
-
-class ViscosityResponse(Model):
-    viscosity: float
-
-
-class ErrorResponse(Model):
-    error: str
-
-
-# Define Request, Response and Error Models for the User Agent
-
-class QuerySkinRequest(Model):
-    day: str
-
-
-class QuerySkinResponse(Model):
-    hydration: int
-    elasticity: float
-    collagen_density: float
-
-class ErrorResponse(Model):
-    error: str
-
-
-# Define Request, Response and Error Models for the Smarthome data Agent
-class EnvironmentRequest(Model):
-    storage_place: str
-
-
-class EnvironmentResponse(Model):
-    temperature: int
-    humidity: int
-    day: str
-
-
-class ErrorResponse(Model):
-    error: str
-
+#The Agents:
 
 SkinAppAgent = Agent(name="SkinAppAgent", seed="skin recovery phrase")
 
@@ -137,65 +106,106 @@ UserAgent = Agent(name="UserAgent", seed="skin_data_day1")
 
 ProductAgent = Agent(name="ProductAgent", seed="productdata_day1")
 
-EnvironmentAgent = Agent(name="EnvironmentAgent", seed="environmentaldata_day1")
+# Define addresses:
 
-DensityAppAgent = Agent(name="DensityAppAgent", seed="density recovery phrase")
-
-ViscosityAppAgent = Agent(name="ViscosityAppAgent", seed="viscosity recovery phrase")
-
-
+skin_address = "agent1q2u49qvajqzpds9yhm5f3y8wz86dhumxmqlusa6rxmxg557uj5sw55p844t"
+user_address ="agent1q0wzslgquuhfykzsyj053jrtuf5vu7caftm7ghjv0vc7v8n0q8x25nuswcq"
+product_address ="agent1qwmdwqt5adeduup748zvc0mfzxat3r4a3ssh5yk0fuzertza6df5yr4x2qy"
 
 #Registration on Almanac
 fund_agent_if_low(SkinAppAgent.wallet.address())
 fund_agent_if_low(ProductAgent.wallet.address())
 fund_agent_if_low(UserAgent.wallet.address())
-fund_agent_if_low(DensityAppAgent.wallet.address())
-fund_agent_if_low(ViscosityAppAgent.wallet.address())
-fund_agent_if_low(EnvironmentAgent.wallet.address())
+
+#Initiation of the three agents
+
+@UserAgent.on_event("startup")
+async def user_details(ctx: Context):
+    ctx.storage.set("day", "0")
+    ctx.storage.set("hydration", 12)
+    ctx.storage.set("elasticity", 0.04)
+    ctx.storage.set("collagen", 2.10)
+    ctx.logger.info(f'User Agent Address is {UserAgent.address}')
+
+@SkinAppAgent.on_event("startup")
+async def skinapp_details(ctx: Context):
+    ctx.storage.set("day", "0")
+    ctx.storage.set("hydration", 0)
+    ctx.storage.set("elasticity", 0)
+    ctx.storage.set("collagen_density", 0)
+    ctx.storage.set("absorption_speed", 0)
+    ctx.storage.set("absorption_level", 0)
+    ctx.storage.set("product_brand", "0")
+    ctx.storage.set("product_name", "0")
+    ctx.storage.set("ingredients_name", "0")
+    ctx.storage.set("ingredients_formula", "0")
+    ctx.storage.set("density", 0)
+    ctx.storage.set("viscosity", 0)
+    ctx.storage.set("storage_area", "0")
+    ctx.storage.set("temperature", "0")
+    ctx.storage.set("humidity", "0")
+    ctx.logger.info(f' Skin Agent Address is {SkinAppAgent.address}')
+
+@ProductAgent.on_event("startup")
+async def product_details(ctx: Context):
+    ctx.storage.set("day", "0")
+    ctx.storage.set("brand", "0")
+    ctx.storage.set("product", "0")
+    ctx.storage.set("ingredients", [])
+    ctx.storage.set("formula", [])
+    ctx.logger.info(f' Product Agent Address is {ProductAgent.address}')
+
+# Establishing agent-specific protocols
+
+user_query_proto = Protocol()
+user_product_add = Protocol()
+main_app_query_proto = Protocol()
+
+# The UserAgent adds a new poduct to the SkinApp Agent's database:
+
+@user_product_add.on_query(model=AppDataRequest)
+def introduce_product(ctx:Context, sender:str, msg:AppDataRequest):
+    with open("agent1q2u49qvajq_data.json.json") as file:
+        query_records = json.load(file)
+        current_day = datetime.fromisoformat(day)
+        brand = input("Insert new product brand")
+        product = input("Insert new product name")
+        query_records["day"].append(current_day)
+        query_records["brand"].append(brand)
+        query_records["product"].append(product)
+
+#The SkinApp agent sending a request to the Product Agent
+
+@main_app_query_proto.on_message(model=FormulaRequest)
+async def handle_product_queru_request(ctx:Context, sender:str, msg:AppDataRequest):
+    brand_query = input("Please provide the product's brand")
+    with open("agent1q2u49qvajq_data.json.json") as file:
+        query_records = json.load(file)
+        if query_records["brand"] == brand_query:
+            ctx.logger.info(f'Fetching user skin status for {msg.brand}')
+            await ctx.send(product_address, FormulaRequest(brand=brand_query))
+
+
+@main_app_query_proto.on_message(model=FormulaResponse)
+async def handle_product_query_response(ctx:Context, sender:str, msg:AppDataRequest):
+    with open("csvjson.json") as file:
+        skincare_data = json.load(file)
+        for item in skincare_data:
+            if item["brand"] == brand:
+                ingredients = item['ingredients']
+                ctx.logger.info(f'Fetching ingredients for {msg.brand} ')
+                ctx.storage.set("ingredients", msg.ingredients)
 
 
 
-# The ProductAgent, the Viscosity App Agent, the Density App Agent, and the EnvironmentalAgent
-# send their data to the Skin App every 28 days (the recommended interval for
-# verifying the skin effectiveness of creams)
 
-app_data_request = Protocol(name="app_data_request")
+UserAgent.include(user_query_proto)
+UserAgent.include(user_product_add)
+SkinAppAgent.include(main_app_query_proto)
+SkinAppAgent.include(user_product_add)
+ProductAgent.include(user_product_add)
 
-
-@app_data_request.on_message(model=DataRequest, replies=DataResponse)
-async def handle_request(ctx: Context, sender: str, msg: DataRequest):
-    await ctx.send(
-        sender, DataResponse(text=f"Please return the requested data {ctx.agent.name}")
-    )
-
-ProductAgent.include(app_data_request)
-DensityAppAgent.include(app_data_request)
-ViscosityAppAgent.include(app_data_request)
-EnvironmentAgent.include(app_data_request)
-
-
-
-
-
-@SkinAppAgent.on_interval(period=1.0)
-async def send_data(ctx: Context):
-    status_list = await ctx.broadcast(app_data_request.digest, message=DataRequest)
-    ctx.logger.info(f"Send to {len(status_list)} agents.")
-
-@SkinAppAgent.on_message(model=DataResponse)
-async def handle_response(ctx: Context, sender: str, msg: DataResponse):
-    ctx.logger.info(f"Received response from {sender}: {msg.day}")
-
-# Request by the User Agent towards the Skin App to retrive all collected data
-
-bureau = Bureau(port=8007)
-bureau.add(SkinAppAgent)
-bureau.add(UserAgent)
-bureau.add(ProductAgent)
-bureau.add(EnvironmentAgent)
-bureau.add(DensityAppAgent)
-bureau.add(ViscosityAppAgent)
 
 if __name__ == '__main__':
-    bureau.run()
+    SkinAppAgent.run()
 
